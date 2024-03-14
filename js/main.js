cc4f71e6764156a56704096e02f35c7213c2e5ad
Jakub <jakubpachut@gmail.com>
2024-03-14 20:05:31 +0100

    added section news

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const burgerBtn = document.querySelector('.nav__burgerBtn')
+const nav = document.querySelector('.nav__items')
+const navItems = document.querySelectorAll('.nav__items-link')
+const loaderContainer = document.querySelector('.loader-container')
+
+const toggleNav = () => {
+	nav.classList.toggle('show')
+
+	navItems.forEach(item => {
+		item.classList.toggle('show')
+
+		item.addEventListener('click', () => {
+			nav.classList.remove('show')
+			navItems.forEach(items => items.classList.remove('show'))
+		})
+	})
+}
+
+// window.addEventListener('load', () => {
+// 	loaderContainer.classList.add('hide')
+// })
+
+burgerBtn.addEventListener('click', toggleNav)
